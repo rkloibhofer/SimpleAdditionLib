64e9458058768444f06b92c69cb28544bfa7557c
rkloibhofer <richard.kloibhofer@slm-software.com>
2016-06-15 09:20:11 +0200

    First Checking

---
+++ Addition/Addition/Addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    public static class Addition
+    {
+		public static int Summation(int firstSummand, int secondSummand)
+		{
+			return firstSummand + secondSummand;
+		}
+    }
+}
